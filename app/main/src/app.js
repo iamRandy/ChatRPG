66c9e19bd80e73bc9c042dc0e381f6136f9c2d46
Randy T <randythai360@gmail.com>
2024-10-29 23:39:24 -0500

    will display text you input

--- app/main/src/app.js
+++ app/main/src/app.js
@@ -1,1 +1,49 @@
-import 'bootstrap';
+var currentContent = "";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textArea = document.getElementById("text_output_area");
+    const input = document.getElementById("text_input");
+    const sendButton = document.getElementById("send_button");
+
+    sendButton.addEventListener("touchend", (event) => {
+        if (input.value == "") return;
+        input.value = "";
+        input.blur();
+
+        const newChat = document.createElement("div");
+        newChat.className = "bg-secondary rounded-4 d-inline-block px-3 py-1 my-1";
+        newChat.textContent = currentContent;
+        textArea.appendChild(newChat);
+        currentContent.textContent = "";
+    })
+    
+    sendButton.addEventListener("mousedown", (event) => {
+        if (input.value == "") return;
+        input.value = "";
+        input.blur();
+
+        const newChat = document.createElement("div");
+        newChat.className = "bg-secondary rounded-4 d-inline-block px-3 py-1 my-1";
+        newChat.textContent = currentContent;
+        textArea.appendChild(newChat);
+        currentContent.textContent = "";
+    })
+
+    input.addEventListener("input", (event) => {
+        currentContent = event.target.value;
+    });
+
+    input.addEventListener("keydown", (event) => {
+        if(input.value == "") return;
+        if(event.key == "Enter") {
+            input.value = "";
+            input.blur();
+            
+            const newChat = document.createElement("div");
+            newChat.className = "bg-secondary rounded-4 d-inline-block px-3 py-1 my-1";
+            newChat.textContent = currentContent;
+            textArea.appendChild(newChat);
+            currentContent.textContent = "";
+        }
+    })
+});
